e7c494fdce5357f752fc3b12490cb1144d1ea216
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-10-22 18:32:43 +0300

    Mahmud Bekkar ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+int x = get_int("guess a number between 1 and 5\n");
+if( x<= 1  ||  x>= 5){
+  printf(" is got right\n");
+}
+  else {
+    printf("is got wrong\n");
+
+  }
+
+
+
+}
 
