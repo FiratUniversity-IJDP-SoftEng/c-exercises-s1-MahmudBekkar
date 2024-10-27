0c49b6728b1404d7d98a190857e6254e340635f8
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-10-27 11:02:58 +0300

    Happy Birthday Counter ex2.c
    Mahmud Bekkar

--- ex2.c
+++ ex2.c
@@ -1,1 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int y = get_int("How old are you");
+    for (int i = 0; i <y; i++)
+
+    {
+        printf("Happy Birthday!\n");
+    }
+}
