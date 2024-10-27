40d0ad96cc7953017da8a98c455118d395561fa8
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-10-27 11:23:12 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
-
 int main() {
     string y = get_string("Do you have a cat or a dog? ");
-
 if (strcmp(y, "cat") == 0)
  {
 printf("The cat says: Meow!!\n");
  }
-
 else if (strcmp(y, "dog") == 0)
  {
   printf("The dog says: Aoww!!\n");
