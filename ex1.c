f62b6d1bc36f9ad08ba2eb8baf8739d391b12112
MahmudBekkar <mahmud.bekkar99@gmail.com>
2024-10-27 11:16:21 +0300

    The Number Guessing Game ex1.c
    Mahmud Bekkar

--- ex1.c
+++ ex1.c
@@ -1,17 +1,15 @@
+
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void){
 int x = get_int("guess a number between 1 and 5\n");
-if( x<= 1  ||  x>= 5){
-  printf(" is got right\n");
+if(x < 1 || x > 5){
+ printf(" you are out of the range of guess\n");
 }
-  else {
-    printf("is got wrong\n");
-
+  else if (x ==3){
+  printf("is got right\n");
   }
-
-
-
-}
-
+  else
+   printf("is got wrong");
+  }
